4fa1971031373f3370c328dbef8f35f0a7e10ed4
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-08-17 15:36:15 +0200

    add config and service for connection with twitter

---
+++ twitter-to-kafka-service/src/main/java/com/microservices/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,33 @@
+package com.microservices.twitter.to.kafka.service;
+
+import com.microservices.twitter.to.kafka.service.config.TwitterToKafkaServiceConfigData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+
+@SpringBootApplication
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
+    private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+
+    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData configData){
+        this.twitterToKafkaServiceConfigData= configData;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOG.info("App starts...");
+        LOG.info(this.twitterToKafkaServiceConfigData.toString());
+        LOG.info(this.twitterToKafkaServiceConfigData.getWelcomeMessage());
+    }
+}
