33cd0cdda5cbabab9ae8e49962938c5265cac188
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-08-19 13:54:27 +0200

    create implementation for admin kafka setup and creation topics

--- app-config-data/src/main/java/com/microservices/twitter/to/kafka/service/config/RetryConfigData.java
+++ app-config-data/src/main/java/com/microservices/twitter/to/kafka/service/config/RetryConfigData.java
@@ -1,2 +1,16 @@
-package com.microservices.twitter.to.kafka.service.config;public class RetryConfigData {
+package com.microservices.twitter.to.kafka.service.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "retry-config")
+public class RetryConfigData {
+    private Long initialIntervalMs;
+    private Long maxIntervalMs;
+    private Double multiplier;
+    private Integer maxAttempts;
+    private Long sleepTimeMs;
 }
